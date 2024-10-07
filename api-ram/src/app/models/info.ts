2dcc039579232697ee6ab0e81df964e2171f0787
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2024-10-07 13:19:04 -0300

    api de rick and morty

---
+++ api-ram/src/app/models/info.ts
@@ -0,0 +1,13 @@
+export class Info {
+    count: number;
+    pages: number;
+    next: string;
+    prev: string;
+
+    constructor(count: number, pages: number, next: string, prev: string) {
+        this.count = count;
+        this.pages = pages;
+        this.next = next;
+        this.prev = prev;
+    }
+}
