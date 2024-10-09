44a1c56ebb57218769c151c857876c5603b1c1e4
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2024-10-09 09:04:11 -0300

    json server

--- example-api/src/app/models/character.response.ts
+++
@@ -1,9 +0,0 @@
-import { Character } from "./character";
-
-export class CharacterResponse {
-    results: Character[];
-
-    constructor(results: Character[]) {
-        this.results = results;
-    }
-}
--- rm-api/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'rm-api',
-  webDir: 'www'
-};
-
-export default config;
